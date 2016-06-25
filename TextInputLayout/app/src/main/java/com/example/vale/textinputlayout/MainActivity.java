69c269cd96158aa2d5586d4c68250e9e4f8b786f
root <root@debian.vale>
2016-06-25 18:27:47 +0200

    1st commit

---
+++ TextInputLayout/app/src/main/java/com/example/vale/textinputlayout/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.vale.textinputlayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        EditText cajatextomail = (EditText)findViewById(R.id.cajamail);
+        cajatextomail.setOnFocusChangeListener(new FocusListener(this));
+    }
+}
